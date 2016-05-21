0e4b7cde924cdb850b09a462a832d21dfb876886
JavaDevMatt <Matt@JavaDevMatt.pl>
2016-05-21 09:58:07 +0200

    init

---
+++ desktop/src/pl/javadevmatt/tutorialclicker/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pl.javadevmatt.tutorialclicker.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pl.javadevmatt.tutorialclicker.TutorialClickerGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TutorialClickerGame(), config);
+	}
+}
