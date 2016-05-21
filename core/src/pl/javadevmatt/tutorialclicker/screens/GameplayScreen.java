820782c9f75276c8271e8d1e576c59e56902ed9d
JavaDevMatt <Matt@JavaDevMatt.pl>
2016-05-21 16:59:17 +0200

    Displaying Player Image

--- core/src/pl/javadevmatt/tutorialclicker/screens/GameplayScreen.java
+++ core/src/pl/javadevmatt/tutorialclicker/screens/GameplayScreen.java
@@ -1,8 +1,11 @@
 package pl.javadevmatt.tutorialclicker.screens;
 
 import pl.javadevmatt.tutorialclicker.TutorialClickerGame;
+import pl.javadevmatt.tutorialclicker.entities.Player;
 
 public class GameplayScreen extends AbstractScreen{
+	
+	private Player player;
 
 	public GameplayScreen(TutorialClickerGame game) {
 		super(game);
@@ -10,7 +13,26 @@ public class GameplayScreen extends AbstractScreen{
 	}
 
 	private void init() {
+		initPlayer();
+	}
+
+	private void initPlayer() {
+		player = new Player();
+		stage.addActor(player);
+	}
+	
+	@Override
+	public void render(float delta) {
+		super.render(delta);
+		update();
 		
+		spriteBatch.begin();
+		stage.draw();
+		spriteBatch.end();
+	}
+
+	private void update() {
+		stage.act();
 	}
 
 }
