16d27a29614f0da10a6c73b9ae37af9b1a7f45db
JavaDevMatt <Matt@JavaDevMatt.pl>
2016-05-21 16:42:38 +0200

    added dummy GameplayScreen

---
+++ core/src/pl/javadevmatt/tutorialclicker/screens/GameplayScreen.java
@@ -0,0 +1,16 @@
+package pl.javadevmatt.tutorialclicker.screens;
+
+import pl.javadevmatt.tutorialclicker.TutorialClickerGame;
+
+public class GameplayScreen extends AbstractScreen{
+
+	public GameplayScreen(TutorialClickerGame game) {
+		super(game);
+		init();
+	}
+
+	private void init() {
+		
+	}
+
+}
