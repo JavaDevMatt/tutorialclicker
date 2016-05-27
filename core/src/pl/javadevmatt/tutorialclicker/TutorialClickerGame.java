5995133311d608694bded3df7415683beb988ee6
JavaDevMatt <Matt@JavaDevMatt.pl>
2016-05-27 08:44:02 +0200

    saving player score

--- core/src/pl/javadevmatt/tutorialclicker/TutorialClickerGame.java
+++ core/src/pl/javadevmatt/tutorialclicker/TutorialClickerGame.java
@@ -1,10 +1,17 @@
 package pl.javadevmatt.tutorialclicker;
 
+
+
 import pl.javadevmatt.tutorialclicker.screens.SplashScreen;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
 
 public class TutorialClickerGame extends Game {
+	
+	public final static String GAME_PREFS = "pl.javadevmatt.preclicker.prefs";
+	public final static String GAME_SCORE = "pl.javadevmatt.preclicker.prefs.score";
 	
 	public final static String GAME_NAME = "Tutorial Clicker";
 	
@@ -13,15 +20,29 @@ public class TutorialClickerGame extends Game {
 	
 	private boolean paused;
 	
+	private Preferences prefs;
+	
 	private int points;
 	
 	@Override
 	public void create () {
+		init();
 		this.setScreen(new SplashScreen(this));
 	}
 	
+	private void init() {
+		prefs =  Gdx.app.getPreferences(GAME_PREFS);
+		loadScore();
+	}
+
+	private void loadScore() {
+		points = prefs.getInteger(GAME_SCORE);
+	}
+
 	public void addPoint(){
 		points++;
+		prefs.putInteger(GAME_SCORE, points);
+		prefs.flush();
 	}
 
 	/**
