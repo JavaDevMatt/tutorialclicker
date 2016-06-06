759d2a298c39d276d7815ac6f474e25a927ce189
JavaDevMatt <Matt@JavaDevMatt.pl>
2016-06-06 18:49:15 +0200

    added ui-red atlas for ResetScoreButton

--- core/src/pl/javadevmatt/tutorialclicker/ui/ResetScoreButton.java
+++ core/src/pl/javadevmatt/tutorialclicker/ui/ResetScoreButton.java
@@ -1,13 +1,16 @@
 package pl.javadevmatt.tutorialclicker.ui;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 public class ResetScoreButton extends Button{
 	
 	public ResetScoreButton(final IClickCallback callback){
-		super(new ButtonStyle());
+		super(prepareResetButtonStyle());
 		
 		init(callback);
 	}
@@ -17,7 +20,6 @@ public class ResetScoreButton extends Button{
 		this.setHeight(100);
 		this.setX(330);
 		this.setY(560);
-		this.setDebug(true);
 		
 		this.addListener(new ClickListener(){
 			@Override
@@ -28,5 +30,15 @@ public class ResetScoreButton extends Button{
 			}
 		});
 	}
+	
+	private static ButtonStyle prepareResetButtonStyle() {
+		TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("ui-red.atlas"));
+		Skin skin = new Skin(atlas);
+		ButtonStyle buttonStyle = new ButtonStyle();
+		buttonStyle.up = skin.getDrawable("button_02");
+		buttonStyle.down = skin.getDrawable("button_03");
+		
+		return buttonStyle;
+	} 
 
 }
