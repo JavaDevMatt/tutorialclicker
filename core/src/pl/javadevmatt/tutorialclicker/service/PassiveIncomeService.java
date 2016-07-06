c28b149713bd3520a1e0063716dbec827f59cdbd
JavaDevMatt <Matt@JavaDevMatt.pl>
2016-07-06 13:38:58 +0200

    adding points based on passive income while app was closed

--- core/src/pl/javadevmatt/tutorialclicker/service/PassiveIncomeService.java
+++ core/src/pl/javadevmatt/tutorialclicker/service/PassiveIncomeService.java
@@ -32,9 +32,17 @@ public class PassiveIncomeService {
 		if(savedTimestamp > 0){
 			long millisPassed = TimeUtils.timeSinceMillis(savedTimestamp);
 			long seconds = TimeUnit.MILLISECONDS.toSeconds(millisPassed);
-			System.out.println("Passed seconds:" + seconds);
+			addPointsBasedOnPassedTime(seconds);
 		} else {
 			// do nothing
+		}
+	}
+	
+	private void addPointsBasedOnPassedTime(long seconds){
+		if(seconds > 0){
+			int points = (int) (seconds * scoreService.getPassiveIncome());
+			points = points / 5;
+			scoreService.addPoints(points);
 		}
 	}
 
