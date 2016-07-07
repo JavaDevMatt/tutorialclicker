925fc7ae6c3fc6788382149b8ddcdbaf6e574662
JavaDevMatt <Matt@JavaDevMatt.pl>
2016-07-07 14:16:13 +0200

    added random event dialogs

--- core/src/pl/javadevmatt/tutorialclicker/controllers/RandomEventController.java
+++ core/src/pl/javadevmatt/tutorialclicker/controllers/RandomEventController.java
@@ -1,19 +1,23 @@
 package pl.javadevmatt.tutorialclicker.controllers;
 
 import pl.javadevmatt.tutorialclicker.TutorialClickerGame;
+import pl.javadevmatt.tutorialclicker.ui.BasicDialog;
 
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.Timer;
 import com.badlogic.gdx.utils.Timer.Task;
 
 public class RandomEventController {
 	
-	private static final int RANDOM_TICK_INTERVAL = 5; // TODO: change after initial implementation
-
-	private TutorialClickerGame game;
+	private static final int RANDOM_TICK_INTERVAL = 30; 
 	
-	public RandomEventController(TutorialClickerGame game){
+	private TutorialClickerGame game;
+	private Stage stage;
+	
+	public RandomEventController(TutorialClickerGame game, Stage stage){
 		this.game = game;
+		this.stage = stage;
 		init();
 	}
 	
@@ -50,16 +54,24 @@ public class RandomEventController {
 		}
 	}
 	
+	private void triggerDialog(String text){
+		BasicDialog basicDialog = new BasicDialog();
+		basicDialog.showDialog(stage, text);
+	}
+	
 	private void gainPassiveIncome() {
 		game.getScoreService().addPassiveIncome();
+		triggerDialog("Yaaaay! You gained passive income!");
 	}
 
 	private void gainMoneyEvent(){
 		game.getScoreService().addPoints(123);
+		triggerDialog("Free money! Yayyy!");
 	}
 	
 	private void loseMoneyEvent(){
 		game.getScoreService().addPoints(-123);
+		triggerDialog("Pay taxes! You owl!");
 	}
 	
 }
